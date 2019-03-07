a645d76c88421fff4df3cb26df26d3f909f1346d
Jeff M <jkdmarks@gmail.com>
2019-03-07 18:01:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,10 @@ katz_deli = []
 def line(current_line)
   if current_line.length == 0
     puts "The line is currently empty."
+  else
+    line_string = "The line is currently: "
+    for person in current_line
+      
+    end
   end
 end
