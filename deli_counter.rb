265145fe1f2dc80def913c03cd83209a7db394db
Jeff M <jkdmarks@gmail.com>
2019-03-07 18:08:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,7 @@ def take_a_number(current_line, name)
   current_line.push(name)
   puts "Welcome, #{name}. You are number #{current_line.length} in line."
 end
+
+def now_serving(current_line)
+  if current_line.length == 0
+end
